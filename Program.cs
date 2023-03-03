ebc9a19599cb86408be22985106c619839ae12d6
KM Musshafen <kmusshafen@hotmail.com>
2023-03-02 19:23:04 -0500

    Create both Dino Class and Display Greeting

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 
 namespace JurassicPark
+
 {
+    class Dinosaur
+    {
+        public string Name { get; set; }
+        public string Diet { get; set; }
+        public int Weight { get; set; }
+        public int Enclosure { get; set; }
+        public int Acquired { get; set; }
+
+        //does this return date.time?
+
+
+
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void DisplayGreeting()
         {
             Console.WriteLine("-------------------------");
             Console.WriteLine("*************************");
@@ -15,6 +29,14 @@ namespace JurassicPark
             Console.WriteLine("-------------------------");
             Console.WriteLine("*************************");
             Console.WriteLine("-------------------------");
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
 
         }
     }
