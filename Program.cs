e456d53c71bc61814787fe0f0387e70ce703439f
KM Musshafen <kmusshafen@hotmail.com>
2023-03-02 19:07:29 -0500

    Inital Commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@
 
 namespace JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("*************************");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Welcome to Jurassic Park!");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("*************************");
+            Console.WriteLine("-------------------------");
+
+        }
     }
-  }
 }
