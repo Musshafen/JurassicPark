2e85b235972e8f15603c588f0ce7e308d219fbcc
KM Musshafen <kmusshafen@hotmail.com>
2023-03-09 19:31:22 -0500

    DinoClass

--- Program.cs
+++ Program.cs
@@ -2,16 +2,7 @@
 
 namespace JurassicPark
 {
-    class Dinosaur
-    {
-        public string Name { get; set; }
-        public string DietType { get; set; }
-        public DateTime WhenAcquired { get; set; }
-        public int Weight { get; set; }
-        public int EnclosureNumber { get; set; }
 
-
-    }
 
     class Program
     {
