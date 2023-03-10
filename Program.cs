dd98d53190d533868ef7b37dba64844e74e4e6ca
KM Musshafen <kmusshafen@hotmail.com>
2023-03-09 19:27:58 -0500

    Trying things

--- Program.cs
+++ Program.cs
@@ -1,56 +1,84 @@
 ﻿using System;
 
 namespace JurassicPark
-
 {
     class Dinosaur
     {
         public string Name { get; set; }
         public string DietType { get; set; }
+        public DateTime WhenAcquired { get; set; }
         public int Weight { get; set; }
         public int EnclosureNumber { get; set; }
-        public DateTime WhenAcquired { get; set; }
-
-        public void DisplayDinosaurs()
-        {
-            Console.WriteLine($"Name: {Name} ");
-            Console.WriteLine($"Diet: {DietType} ");
-            Console.WriteLine($"Weight: {Weight} ");
-            Console.WriteLine($"Acquired: {WhenAcquired} ");
-            Console.WriteLine($"Enclosure: {EnclosureNumber} ");
-            Console.WriteLine();
-
-
-
-        }
-
 
 
     }
+
     class Program
     {
         static void DisplayGreeting()
         {
-            Console.WriteLine("-------------------------");
-            Console.WriteLine("*************************");
-            Console.WriteLine("-------------------------");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("****************************************");
+            Console.WriteLine("    Welcome to Jurassic Park   ");
+            Console.WriteLine("****************************************");
+            Console.WriteLine("----------------------------------------");
             Console.WriteLine();
-            Console.WriteLine("Welcome to Jurassic Park!");
             Console.WriteLine();
-            Console.WriteLine("-------------------------");
-            Console.WriteLine("*************************");
-            Console.WriteLine("-------------------------");
-
+            Console.WriteLine("                 ...Life uh, finds a way.");
         }
 
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
 
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }
 
         static void Main(string[] args)
         {
+            var dinosaur = new Dinosaur();
+
             DisplayGreeting();
 
+            var keepGoing = true;
 
+            while (keepGoing)
+            {
+                Console.WriteLine();
+                Console.Write("What do you want to do (A)dd a dinosaur or (Q)uit: ");
+                var choice = Console.ReadLine().ToUpper();
 
+                if (choice == "Q")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    var dinosaur = new Dinosaur();
+
+                    dinosaur.Name = PromptForString("What is your name? ");
+                    dinosaur.DietType = PromptForString("")
+
+                }
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # JurassicPark
+
+using System;
+
+namespace JurassicPark
+
+{
+class Dinosaur
+{
+public string Name { get; set; }
+public string DietType { get; set; }
+public int Weight { get; set; }
+public int EnclosureNumber { get; set; }
+public DateTime WhenAcquired { get; set; }
+
+        public void DisplayDinosaurs()
+        {
+            Console.WriteLine($"Name: {Name} ");
+            Console.WriteLine($"Diet: {DietType} ");
+            Console.WriteLine($"Weight: {Weight} ");
+            Console.WriteLine($"Acquired: {WhenAcquired} ");
+            Console.WriteLine($"Enclosure: {EnclosureNumber} ");
+            Console.WriteLine();
+
+        }
+
+    }
+
+    class DinosaurDatabase
+    {
+
+        public List<Dinosaur> Dinosaurs { get; set; } = new List<Dinosaur>();
+
+        public void AddDinosaur(Dinosaur add)
+        {
+            Dinosaurs.Add(add);
+        }
+        public void RemoveDinosaur(Dinosaur remove)
+        {
+            Dinosaurs.Remove(remove);
+        }
+        public Dinosaur ViewOneDinosaur(string dinoToFind)
+        {
+            Dinosaur foundDinosaur = Dinosaurs.FirstOrDefault(dinosaur => dinosaur.Name.ToUpper().Contains(dinoToFind.ToUpper()));
+            return foundDinosaur;
+        }
+        public List<Dinosaur> ViewAllDionsaurs()
+        {
+            return Dinosaurs;
+        }
+        public void LoadDinosaur()
+        {
+            if (File.Exists("Dinosaur.csv"))
+            {
+                var fileReader = new StreamReader("Dinosaurs.cvs");
+
+            }
+        }
+
+    }
+
+}
+
+class Program
+{
+static void DisplayGreeting()
+{
+Console.WriteLine("-------------------------");
+Console.WriteLine("************\*************");
+Console.WriteLine("-------------------------");
+Console.WriteLine();
+Console.WriteLine("Welcome to Jurassic Park!");
+Console.WriteLine();
+Console.WriteLine("-------------------------");
+Console.WriteLine("************\*************");
+Console.WriteLine("-------------------------");
+
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        DisplayGreeting();
+
+        var database = new DinosaurDatabase();
+
+
+    }
+
+
+}
+}
