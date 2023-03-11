4903059a2b569c4bc35bd09903b186af560546de
KM Musshafen <kmusshafen@hotmail.com>
2023-03-11 12:33:38 -0500

    Working Code Say What?! Welcome to Jurassic Park

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿using System;
 
 namespace JurassicPark
+
 {
+
+    class Dinosaur
+    {
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public DateTime WhenAcquired { get; set; }
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
+
+    }
 
 
     class Program
@@ -45,10 +56,17 @@ namespace JurassicPark
 
         static void Main(string[] args)
         {
-            //var dinosaur = new Dinosaur();
+            var dinosaur = new Dinosaur();
 
             DisplayGreeting();
 
+            dinosaur.Name = PromptForString("What is the name of the dinosaur? ");
+            dinosaur.DietType = PromptForString("Is it a (C)arnivore or a (H)erbivore? ").ToUpper();
+            dinosaur.WhenAcquired = DateTime.Now;
+            dinosaur.Weight = PromptForInteger("How much does the dino weigh, in pounds? ");
+            dinosaur.EnclosureNumber = PromptForInteger("Please assign this dino to an enclosure number: ");
+
+            Console.WriteLine($"Your Dino DNA says, {dinosaur.Name} is a {dinosaur.DietType} and is in enclosure: {dinosaur.EnclosureNumber}.");
 
 
         }
